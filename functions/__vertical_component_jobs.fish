bde793f378600c86aaf43543e72180692412fcdd
Adam Christiansen <code@adamc.io>
2019-07-24 17:58:34 -0600

    feat: add jobs indicator

---
+++ functions/__vertical_component_jobs.fish
@@ -0,0 +1,16 @@
+function __vertical_component_jobs -d "Print the jobs indicator"
+    # Options
+    __vertical_util_set_default VERTICAL_JOBS_COLOR  cyan --bold
+    __vertical_util_set_default VERTICAL_JOBS_PREFIX " "
+    __vertical_util_set_default VERTICAL_JOBS_SHOW   true
+    __vertical_util_set_default VERTICAL_JOBS_SYM    "●"
+
+    [ $VERTICAL_JOBS_SHOW != true ]; and return
+
+    set -l count (jobs | wc -l)
+    [ $count -eq 0 ]; and return
+
+    set_color $VERTICAL_JOBS_COLOR
+    echo -ens $VERTICAL_JOBS_PREFIX $VERTICAL_JOBS_SYM
+    set_color $VERTICAL_NORMAL_COLOR
+end
