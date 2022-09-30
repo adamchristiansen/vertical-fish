8f191f3221760e030f51093603b7812441106b63
Adam Christiansen <code@adamc.io>
2022-09-30 14:16:33 -0600

    performance: general refactor to increase performance

--- functions/__vertical_component_git_status.fish
+++ functions/__vertical_component_git_status.fish
@@ -1,90 +1,86 @@
 function __vertical_component_git_status -d "Print the git status"
-    # Options
-    __vertical_util_set VERTICAL_GIT_STATUS_COLOR         cyan --bold
-    __vertical_util_set VERTICAL_GIT_STATUS_PREFIX        " ["
-    __vertical_util_set VERTICAL_GIT_STATUS_SHOW          true
-    __vertical_util_set VERTICAL_GIT_STATUS_SUFFIX        "]"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_ADDED     "+"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_AHEAD     "↑"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_BEHIND    "↓"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_DIVERGED  "⇵"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_DELETED   "-"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_MODIFIED  "!"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_RENAMED   "»"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_STASHED   "#"
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_UNMERGED  "="
-    __vertical_util_set VERTICAL_GIT_STATUS_SYM_UNTRACKED "?"
+  __vertical_util_set VERTICAL_GIT_STATUS_COLOR         cyan --bold
+  __vertical_util_set VERTICAL_GIT_STATUS_PREFIX        " ["
+  __vertical_util_set VERTICAL_GIT_STATUS_SHOW          true
+  __vertical_util_set VERTICAL_GIT_STATUS_SUFFIX        "]"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_ADDED     "+"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_AHEAD     "↑"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_BEHIND    "↓"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_DIVERGED  "⇵"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_DELETED   "-"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_MODIFIED  "!"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_RENAMED   "»"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_STASHED   "#"
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_UNMERGED  "="
+  __vertical_util_set VERTICAL_GIT_STATUS_SYM_UNTRACKED "?"
 
-    not __vertical_util_is_git; and return
-    [ $VERTICAL_GIT_SHOW != true ]; and return
-    [ $VERTICAL_GIT_STATUS_SHOW != true ]; and return
+  if not __vertical_util_is_git
+    return
+  end
 
-    # This is a list of statuses that are found
-    set -l statuses
-    # Get the git status in a format that can be parsed
-    set -l git_status (command git status --porcelain -b 2> /dev/null)
-    # Get only the first line of the status with the branch information
-    set -l git_status_first (string split \n $git_status)[1]
+  if [ $VERTICAL_GIT_SHOW != true -o $VERTICAL_GIT_STATUS_SHOW != true ]
+    return
+  end
 
-    # Check the status of each file
-    for i in (string split \n $git_status | string sub -s 1 -l 2)
-        switch $i
-            case '*A*';  set -a statuses added
-            case '*D*';  set -a statuses deleted
-            case '*M*';  set -a statuses modified
-            case '*R*';  set -a statuses renamed
-            case '*U*';  set -a statuses unmerged
-            case '*\?*'; set -a statuses untracked
-        end
-    end
+  set -l untracked false
+  set -l added     false
+  set -l deleted   false
+  set -l modified  false
+  set -l renamed   false
+  set -l stashed   false
+  set -l unmerged  false
+  set -l ahead     false
+  set -l behind    false
+  set -l diverged  false
 
-    # Check if there are any files stashed
-    [ -n (echo (command git rev-parse --verify refs/stash 2> /dev/null)) ]
-    and set -a statuses stashed
+  set -l git_status (command git status --porcelain -b 2> /dev/null)
+  set -l git_status_first (string split \n $git_status)[1]
 
-    # Check if the branch is ahead
-    set -l ahead false
-    string match -r '\[.*ahead.*\]' $git_status_first > /dev/null
-    and set ahead true
+  for s in (string split \n $git_status | string sub -s 1 -l 2)
+    string match -q '*A*'  $s && set added     true
+    string match -q '*D*'  $s && set deleted   true
+    string match -q '*M*'  $s && set modified  true
+    string match -q '*R*'  $s && set renamed   true
+    string match -q '*U*'  $s && set unmerged  true
+    string match -q '*\?*' $s && set untracked true
+  end
 
-    # Check if the branch is behind
-    set -l behind false
-    string match -r '\[.*behind.*\]' $git_status_first > /dev/null
-    and set behind true
+  if [ -n (echo (command git rev-parse --verify refs/stash 2> /dev/null)) ]
+    set stashed true
+  end
 
-    # Set whether the branch is ahead, behind, or diverged
-    if [ $ahead = true ] && [ $behind = true ]
-        set -a statuses diverged
-    else if [ $ahead = true ]
-        set -a statuses ahead
-    else if [ $behind = true ]
-        set -a statuses behind
-    end
+  if string match -r '\[.*ahead.*\]' $git_status_first > /dev/null
+    set ahead true
+  end
 
-    # The statuses are made uppercase so that they can be used in variable
-    # substitution
-    set statuses (string upper $statuses)
+  if string match -r '\[.*behind.*\]' $git_status_first > /dev/null
+    set behind true
+  end
 
-    # The order to display the git status symbols
-    set -l order untracked added deleted modified renamed stashed unmerged \
-        ahead behind diverged
+  if [ $ahead = true ] && [ $behind = true ]
+    set diverged true
+    set ahead    false
+    set behind   false
+  end
 
-    # Build up the status symbols by checking whether the status type was
-    # discovered.
-    set -l status_syms
-    for i in $order
-        set i (string upper $i)
-        if contains $i in $statuses
-            set -l status_symbol VERTICAL_GIT_STATUS_SYM_$i
-            set status_syms "$$status_symbol$status_syms"
-        end
-    end
+  set -l status_syms (string join '' \
+    ([ $untracked = true ] && echo $VERTICAL_GIT_STATUS_SYM_UNTRACKED) \
+    ([ $added = true ] && echo $VERTICAL_GIT_STATUS_SYM_ADDED) \
+    ([ $deleted = true ] && echo $VERTICAL_GIT_STATUS_SYM_DELETED) \
+    ([ $modified = true ] && echo $VERTICAL_GIT_STATUS_SYM_MODIFIED) \
+    ([ $renamed = true ] && echo $VERTICAL_GIT_STATUS_SYM_RENAMED) \
+    ([ $stashed = true ] && echo $VERTICAL_GIT_STATUS_SYM_STASHED) \
+    ([ $unmerged = true ] && echo $VERTICAL_GIT_STATUS_SYM_UNMERGED) \
+    ([ $ahead = true ] && echo $VERTICAL_GIT_STATUS_SYM_AHEAD) \
+    ([ $behind = true ] && echo $VERTICAL_GIT_STATUS_SYM_BEHIND) \
+    ([ $diverged = true ] && echo $VERTICAL_GIT_STATUS_SYM_DIVERGED) \
+  )
 
-    if [ -n (echo $status_syms) ]
-        set_color $VERTICAL_GIT_STATUS_COLOR
-        echo -ens $VERTICAL_GIT_STATUS_PREFIX
-        echo -ens $status_syms
-        echo -ens $VERTICAL_GIT_STATUS_SUFFIX
-        set_color $VERTICAL_COLOR_NORMAL
-    end
+  if [ -n "$status_syms" ]
+    set_color $VERTICAL_GIT_STATUS_COLOR
+    echo -ens $VERTICAL_GIT_STATUS_PREFIX
+    echo -ens $status_syms
+    echo -ens $VERTICAL_GIT_STATUS_SUFFIX
+    set_color $VERTICAL_COLOR_NORMAL
+  end
 end
--- functions/__vertical_component_vert.fish
+++ functions/__vertical_component_vert.fish
@@ -1,14 +1,13 @@
 function __vertical_component_vert -d "Print the vertical bar"
-    # Options
-    __vertical_util_set VERTICAL_VERT_PREFIX        "│"
-    __vertical_util_set VERTICAL_VERT_COLOR_SUCCESS green
-    __vertical_util_set VERTICAL_VERT_COLOR_FAILURE red
+  __vertical_util_set VERTICAL_VERT_PREFIX        "│"
+  __vertical_util_set VERTICAL_VERT_COLOR_SUCCESS green
+  __vertical_util_set VERTICAL_VERT_COLOR_FAILURE red
 
-    if [ $__vertical_exit_code -eq 0 ]
-        set_color $VERTICAL_VERT_COLOR_SUCCESS
-    else
-        set_color $VERTICAL_VERT_COLOR_FAILURE
-    end
-    echo -ens $VERTICAL_VERT_PREFIX
-    set_color $VERTICAL_COLOR_NORMAL
+  if [ $__vertical_exit_code -eq 0 ]
+    set_color $VERTICAL_VERT_COLOR_SUCCESS
+  else
+    set_color $VERTICAL_VERT_COLOR_FAILURE
+  end
+  echo -ens $VERTICAL_VERT_PREFIX
+  set_color $VERTICAL_COLOR_NORMAL
 end
