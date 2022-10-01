db65c5350ca6a3355c2041d5f9aa4385b52d1b3b
Adam Christiansen <hello@adamc.io>
2022-10-01 17:00:11 -0600

    refactor: remove awk dependency

--- functions/__vertical_component_duration.fish
+++ functions/__vertical_component_duration.fish
@@ -12,8 +12,39 @@ function __vertical_component_duration -d "Print the duration of the previous co
     return
   end
 
+  set -l t $CMD_DURATION
+  set -l dur
+  if [ $t -eq 0 ]
+    set dur -a '0s'
+  else
+    set -l ms (math --scale 0 -- $t % 1000)
+    set -l t  (math --scale 0 -- $t / 1000)
+    set -l s  (math --scale 0 -- $t % 60)
+    set -l t  (math --scale 0 -- $t / 60)
+    set -l m  (math --scale 0 -- $t % 60)
+    set -l t  (math --scale 0 -- $t / 60)
+    set -l h  (math --scale 0 -- $t % 24)
+    set -l t  (math --scale 0 -- $t / 24)
+    set -l d  $t
+    if [ $d != 0 ]
+      set -a dur {$d}d
+    end
+    if [ $h != 0 ]
+      set -a dur {$h}h
+    end
+    if [ $m != 0 ]
+      set -a dur {$m}m
+    end
+    if [ $s != 0 ]
+      set -a dur {$s}s
+    end
+    if [ $ms != 0 ]
+      set -a dur {$ms}ms
+    end
+  end
+
   echo -ens $VERTICAL_DURATION_PREFIX
   set_color $VERTICAL_DURATION_COLOR
-  echo -ens (echo $CMD_DURATION | __vertical_util_duration)
+  echo -ens (string join ' ' $dur)
   set_color $VERTICAL_COLOR_NORMAL
 end
