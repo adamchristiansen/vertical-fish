35d7d0cd04b2a3c6467eb06dcc6f81ef32c3448b
Adam Christiansen <hello@adamc.io>
2022-10-23 14:45:24 -0600

    fix: make vertical_version global
    Closes #1

--- conf.d/vertical.fish
+++ conf.d/vertical.fish
@@ -1,3 +1,5 @@
+set -g vertical_version 1.0.1
+
 function __vertical_install --on-event vertical_install
   # Nothing to do
 end
@@ -8,4 +10,5 @@ end
 
 function __vertical_uninstall --on-event vertical_uninstall
   vertical_clear_configuration
+  set -eg vertical_version
 end
